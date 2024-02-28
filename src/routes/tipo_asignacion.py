335f3cfa2bf9b5c0bc1b96ce5bd6cd11d773711b
desarrolladorPAG <desarrollador@pagcolombian.com>
2024-02-28 16:16:59 -0500

    Cambio de nombre de funcion

--- src/routes/tipo_asignacion.py
+++ src/routes/tipo_asignacion.py
@@ -6,5 +6,5 @@ tipo_asignacion = Blueprint('tipo_asignacion', __name__, url_prefix='/api/v1')
 
 @cross_origin()
 @tipo_asignacion.route("tipo_asignacion", methods = ['POST'])
-def crear_tipo_adjunto():
+def crear_tipo_asignacion():
     return tipo_asignacion_controller.crear_tipo_asignacion()
