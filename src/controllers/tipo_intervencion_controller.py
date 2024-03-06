64b89ca8534ab8cb7046e0bd8cb97ddb35f786da
desarrolladorPAG <desarrollador@pagcolombian.com>
2024-03-06 11:06:45 -0500

    Modifciacion de funcion crear tipod de intervencion

--- src/controllers/tipo_intervencion_controller.py
+++ src/controllers/tipo_intervencion_controller.py
@@ -3,19 +3,17 @@ from models.tipo_intervenciones import *
 import uuid
 import binascii
 
-def crear_tipo_intervencion():
+def crear_tipo_intervencion(id_tipo_cliente_escogido, nuevo_nombre_tipo_intervencion):
     try:
         id_tipo_intervencion = uuid.uuid4().bytes
-        id_tipo_cliente = request.json["id_tipo_cliente"]
-        nombre_tipo_intervencion = request.json["nombre_tipo_intervencion"]
+        id_tipo_cliente = id_tipo_cliente_escogido
+        nombre_tipo_intervencion = nuevo_nombre_tipo_intervencion
 
-        id_tipo_cliente_bytes = binascii.unhexlify(id_tipo_cliente)
-
-        new_tipo_intervencion = Tipo_intervenciones(id_tipo_intervencion, id_tipo_cliente_bytes, nombre_tipo_intervencion)
+        new_tipo_intervencion = Tipo_intervenciones(id_tipo_intervencion, id_tipo_cliente, nombre_tipo_intervencion)
         db.session.add(new_tipo_intervencion)
         db.session.commit()
 
-        return jsonify({"message" : "Tipo de intervencion creado correctamente", "status" : 200})
+        return id_tipo_intervencion
     
     except Exception as e:
         return jsonify({"message" : "Ha ocurrido un error inesperado", "error" : str(e)}) , 500
